46cb914dd749bf2e57002f8c84cff5c773f65b81
Lukas Valenta <lukas.valenta@applifting.cz>
2020-07-16 08:18:27 +0200

    chore(map-ast): Add contentLanguage to request

--- src/interfaces/ast/map-ast.ts
+++ src/interfaces/ast/map-ast.ts
@@ -62,6 +62,7 @@ export interface HTTPOperationDefinitionNode extends MapASTNodeBase {
   variableExpressionsDefinition: VariableExpressionDefinitionNode[];
   requestDefinition: {
     contentType: string;
+    contentLanguage: string;
     security: 'basic' | 'bearer' | 'other';
     headers: VariableExpressionDefinitionNode[];
     body: MapExpressionDefinitionNode[];
